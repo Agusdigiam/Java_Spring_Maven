173eed72d166ea81ac36b922d33919622bf3a802
Agustin Di Giambatista <agustindigiambatista@gmail.com>
2023-09-04 20:23:38 -0300

    Servicio Rest

--- curso/src/main/java/com/practica/java/curso/controllers/UsuarioController.java
+++ curso/src/main/java/com/practica/java/curso/controllers/UsuarioController.java
@@ -1,12 +1,28 @@
 package com.practica.java.curso.controllers;
 
+import com.practica.java.curso.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class UsuarioController {
-        @RequestMapping(value = "Prueba")
+        @RequestMapping(value = "prueba")
         public String prueba(){
-            return "Esta andando";
+            return "Todo Funciona Correctamente!";
+        }
+        @RequestMapping(value = "acerca")
+        public String acerca(){
+            return "Esta prueba de pagina fue creada por Agustin Di Giambatista en Septiembre del 2023";
+       }
+        @RequestMapping(value = "user")
+        public Usuario getUsuario() {
+            Usuario usuario = new Usuario();
+            usuario.setNombre("Agustin");
+            usuario.setApellido("Di Giambatista");
+            usuario.setEmail("agustindigiambatista@gmail.com");
+            usuario.setTelefono("2302 889966");
+            return usuario;
         }
 }
+
